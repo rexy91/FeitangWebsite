19f80918cf6a5b968e435df20d6894e5ffd72908
Rex Ye <rxye1991@gmail.com>
2020-11-08 13:53:07 -0500

    createdNavbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './Navbar.scss' 
+export default class Navbar extends Component {
+    render() {
+        return (
+
+<nav>
+  <ul>
+    <li><a href="#0">Home</a></li>
+    <li><a href="#0">About</a></li>
+    <li><a href="#0">Clients</a></li>
+    <li><a href="#0">Services</a></li>
+    <li><a href="#0">Contact</a></li>
+  </ul>
+</nav>
+        )
+    }
+}
