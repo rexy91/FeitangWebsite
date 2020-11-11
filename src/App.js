1285bc4b79d910bb38f54363b18a4f51d15c6d4e
Rex Ye <rxye1991@gmail.com>
2020-11-11 17:24:58 -0500

    Generated Divisions for Landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Landing from './Components/Landing/Landing'
 
 export default class App extends Component {
   render() {
     return (
       <div>
           <Navbar/>
+          <Landing/>
       </div>
     )
   }
---
+++ src/Components/Landing/Landing.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Landing.scss' 
+
+export default function Landing() {
+  return (
+    <div className='landing-page'>
+          <div className='paragraph'>
+            <p>FetTang Group Inc. is a leading diversified group committed to providing high quality products and services globally. Since established, our in-depth knowledge of local and international markets, abundant capital and strong financing ability, advanced technology, and commitment to our clients have earned us the trust of customers and partners, making us the leader in supplying trendy products and services locally and internationally. </p>
+          </div>
+          <div className='paragraph'>
+            <p>FetTang Group Inc. is a leading diversified group committed to providing high quality products and services globally. Since established, our in-depth knowledge of local and international markets, abundant capital and strong financing ability, advanced technology, and commitment to our clients have earned us the trust of customers and partners, making us the leader in supplying trendy products and services locally and internationally. </p>
+          </div>
+          <div className='paragraph'>
+            <p>FetTang Group Inc. is a leading diversified group committed to providing high quality products and services globally. Since established, our in-depth knowledge of local and international markets, abundant capital and strong financing ability, advanced technology, and commitment to our clients have earned us the trust of customers and partners, making us the leader in supplying trendy products and services locally and internationally. </p>
+          </div>
+    </div>
+  )
+}
