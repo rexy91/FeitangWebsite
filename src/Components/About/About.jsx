b1dffb3d9dde2b5a7b5d0bb7dd519842e97a9ced
Rex Ye <rxye1991@gmail.com>
2020-11-25 11:38:15 -0500

    commitmentsPageIconFinished

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './About.scss'
 
+import icon1 from '../icons/commitmentOneIcon.jpg'
 
 class About extends Component {
     render() {
@@ -10,18 +11,27 @@ class About extends Component {
                     <h2>About Us</h2>
                     <p>Since FeiTang Group Inc. Established, our in-depth knowledge of local and international markets, abundant capital and strong financing ability, advanced technology, and commitment to our clients have earned us the trust of customers and partners.</p>
                 </div>
+
                 <div id='commitments-div'>
-                    <div>
-                        1
+                    <div className='single-commitment'>
+                         <img className='commitments-image'src="https://www.flaticon.com/svg/static/icons/svg/1161/1161490.svg" alt=""/>
+                         <br/>
+                         Provide the best value and quality products for our customers.
                     </div>
-                    <div>
-                        2
+                    <div className='single-commitment'>
+                         <img className='commitments-image'src="https://www.flaticon.com/svg/static/icons/svg/3126/3126649.svg" alt=""/>
+                         <br/>
+                         Ensure every customer is satisfied through superior service and innovation.
                     </div>
-                    <div>
-                        3
+                    <div className='single-commitment'>
+                         <img className='commitments-image'src="https://www.flaticon.com/svg/static/icons/svg/1063/1063376.svg" alt=""/>
+                         <br/>
+                         Utilize our core business values of honesty, commitment, and integrity to drive all of our decisions.
                     </div>
-                    <div>
-                        4
+                    <div className='single-commitment'>
+                         <img className='commitments-image'src="https://www.flaticon.com/svg/static/icons/svg/1342/1342065.svg" alt=""/>
+                         <br/>
+                         Continue to grow and innovate to maintain customer loyalty.
                     </div>
                 </div>
             </div>
