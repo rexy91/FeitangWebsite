a34d98be0231744f0f875bde536ec291d2a38b3e
Rex Ye <rxye1991@gmail.com>
2020-11-28 17:39:30 -0500

    addedPlaceholderforform

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -12,15 +12,15 @@ class Contact extends Component {
         </h5>
             <div className="md-form mt-3">
                 <input type="text" id="materialContactFormName" class="form-control"/>
-                <label for="materialContactFormName">Name</label>
+                <label for="materialContactFormName" placeholder='Your Name'></label>
             </div>
             <div className="md-form">
                 <input type="email" id="materialContactFormEmail" class="form-control"/>
-                <label for="materialContactFormEmail">E-mail</label>
+                <label for="materialContactFormEmail" placeholder='Your Email'></label>
             </div>
             <div className="md-form">
                 <textarea id="materialContactFormMessage" class="form-control md-textarea" rows="3"></textarea>
-                <label for="materialContactFormMessage">Message</label>
+                <label for="materialContactFormMessage" placeholder='Leave a message....'></label>
             </div>            
             <button className="btn btn-outline-info btn-rounded btn-block z-depth-0 my-4 waves-effect" type="submit">Send</button>
         </form>
